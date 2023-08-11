410761272ac87e6b102f5b8cf13e7ed605cab880
Harishankar Vidhyanandam <harishankarvidhyanandam@Harishankars-MacBook-Air.local>
2023-08-11 23:03:23 +0530

    check all the url in data.csv file

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import csv
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    csv_file_path = 'data.csv' 
+    urls = []
+    with open(csv_file_path, 'r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            name = row[0]
+            url = row[1]
+            urls.append((name, url))  # Append a tuple of (name, url)
+
+    results = []
+    names=[]
+    for name, url in urls:
+        try:
+            response = requests.get(url)
+            status = response.status_code
+            if status == 200:
+                result = f"The URL {url} is reachable and returns a status code 200 (OK)."
+            else:
+                result = f"The URL {url} is reachable but returns a status code {status}."
+        except requests.ConnectionError:
+            result = f"The URL {url} is unreachable."
+            name = f"Project name {name}"
+        names.append(name)
+        results.append(result)  # Append a tuple of (name, result)
+
+    return render_template('index.html', results=results, names=names)
+
+if __name__ == '__main__':
+    app.run(debug=True)
